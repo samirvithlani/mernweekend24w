c5222940bf46b922934d4ccd746ba3eb02dcb042
samirvithlani <samir.vithlani83955@gmail.com>
2024-10-12 17:35:39 +0530

    update

--- src/api/ApiDemo1.jsx
+++ src/api/ApiDemo1.jsx
@@ -5,6 +5,7 @@ import { ToastContainer, toast, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
 import { Modal } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 export const ApiDemo1 = () => {
   const getApiData = async () => {
@@ -17,11 +18,33 @@ export const ApiDemo1 = () => {
     console.log("res", res.data.data);
     setmessage(res.data.message);
     setusers(res.data.data);
-    
   };
   const [message, setmessage] = useState("");
   const [users, setusers] = useState([]);
   const [isLoading, setisLoading] = useState(false);
+  const [show, setshow] = useState(false);
+  const [user, setuser] = useState({});
+  const [updateShow, setupdateShow] = useState(false);
+
+  const { register, handleSubmit,reset } = useForm({
+   defaultValues: user,
+  });
+
+
+
+  const submitHandler = async(data) => {
+    console.log("Data...",data)
+    const id = data._id
+    try{
+    delete data._id
+    const res = await axios.put(`https://node5.onrender.com/user/user/${id}`,data)
+    console.log("res = ",res)
+    }catch(err){
+        console.log("Error...",err)
+    }
+    setupdateShow(false);//pop closer
+    getApiData(); //to fetch updated data from api...
+}
 
   const deleteUser = async (id) => {
     //alert(id)
@@ -48,30 +71,40 @@ export const ApiDemo1 = () => {
     getApiData();
   }, []);
 
-  const [show, setshow] = useState(false)
-  const [user, setuser] = useState({})
-  const handleShow = async(id) => {
-
+  
+  const handleShow = async (id) => {
     const res = await axios.get(`https://node5.onrender.com/user/user/${id}`);
     //console.log("Res...", res);
     setuser(res.data.data);
-    setshow(true)
-  }
+    setshow(true);
+  };
+
+  const handleShowUpdate = async (id) => {
+
+    //fetch clicked user data from api...
+     const res = await axios.get(`https://node5.onrender.com/user/user/${id}`);
+    console.log("Res111...", res);
+    setuser(res.data.data);
+    reset(res.data.data);
+
+    setupdateShow(true);
+  };
+
   const handleClose = () => {
-    setshow(false)
-  }
+    setshow(false);
+  };
 
   //const [searchquery, setsearchquery] = useState("")
   const searchData = async (event) => {
     console.log(event.target.value);
-    const res  = await axios.get(`https://node5.onrender.com/user/filter2`,{
-      params:{
-        name:event.target.value
-      }
+    const res = await axios.get(`https://node5.onrender.com/user/filter2`, {
+      params: {
+        name: event.target.value,
+      },
     });
     console.log("Res...", res);
     setusers(res.data.data);
-  }
+  };
   return (
     <div>
       {/* <input type="search"  onChange={(event)=>{setsearchquery(event.target.value)}}/> */}
@@ -134,7 +167,24 @@ export const ApiDemo1 = () => {
                   >
                     Update
                   </Link>
-                  <button onClick={()=>{handleShow(user._id)}} className="btn btn-info" style={{marginLeft:"10px"}}>info</button>
+                  <button
+                    onClick={() => {
+                      handleShow(user._id);
+                    }}
+                    className="btn btn-info"
+                    style={{ marginLeft: "10px" }}
+                  >
+                    info
+                  </button>
+                  <button
+                    onClick={() => {
+                      handleShowUpdate(user._id);
+                    }}
+                    className="btn btn-info"
+                    style={{ marginLeft: "10px" }}
+                  >
+                    update 1
+                  </button>
                 </td>
               </tr>
             );
@@ -142,18 +192,47 @@ export const ApiDemo1 = () => {
         </tbody>
       </table>
       <Modal show={show}>
-            <Modal.Header>
-              <Modal.Title>User Detail</Modal.Title>
-              <button onClick={handleClose}>Close</button>
-            </Modal.Header>
-            <Modal.Body>
-              <h4>User Name ={user.name}</h4>
-              <h4>User Email ={user.email}</h4>
-              <h4>User Age ={user.age}</h4>
-              </Modal.Body>
-              <Modal.Footer>
-                <p>Footer</p>
-              </Modal.Footer>
+        <Modal.Header>
+          <Modal.Title>User Detail</Modal.Title>
+          <button onClick={handleClose}>Close</button>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>User Name ={user.name}</h4>
+          <h4>User Email ={user.email}</h4>
+          <h4>User Age ={user.age}</h4>
+        </Modal.Body>
+        <Modal.Footer>
+          <p>Footer</p>
+        </Modal.Footer>
+      </Modal>
+
+      {/* //update model */}
+      <Modal show={updateShow}>
+        <Modal.Header>
+          <Modal.Title>Update User</Modal.Title>
+          <button
+            onClick={() => {
+              setupdateShow(false);
+            }}
+          >
+            Close
+          </button>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit(submitHandler)}>
+            <div>
+              <label>NAME</label>
+              <input type="text" {...register("name")} />
+            </div>
+            <div>
+              <label>AGE</label>
+              <input type="text" {...register("age")} />
+            </div>
+            <div>
+              <input type="submit" value="UPDATE" />
+            </div>
+          </form>
+        </Modal.Body>
       </Modal>
     </div>
   );
